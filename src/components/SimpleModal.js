f36f07d9dbe4ae5e864229e7314b5af670e09649
Sandbagger <wneal_19@yahoo.co.uk>
2019-01-20 19:22:46 +0000

    Work around: Set up nasa_id route param in App for modal, within ImageGrid buttons Link to url containing nasa_id, modal accesses nasa_id route parmam to access data and render correct content

--- src/components/SimpleModal.js
+++ src/components/SimpleModal.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter,  Route } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 import { withStyles } from '@material-ui/core/styles';
-import queryString from 'query-string';
+
 
 function getModalStyle() {
   const top = 50;
@@ -27,28 +28,43 @@ const styles = theme => ({
   },
 });
 
+class SimpleModal extends Component{
+ constructor(props){
+   super()
+   this.state = {open: true}
+ }
 
-function SimpleModal(props){
-  const { classes } = props;
+ componentDidMount() {
+  const { match: { params } } = this.props;
 
+  console.log(this.props.match.params);
+}
+
+
+ render(){
+  const { classes } = this.props;
+  const nasaid = this.props.match.params.nasaid;
+  console.log(nasaid, 'test')
+console.log(this.props)
     return (<Modal
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
-        open={props.isOpen}
-        onClose={props.close}
+        open={this.props.isOpen}
+        onClose={this.props.close}
       >
         <div style={getModalStyle()}  className={classes.paper}>
           <Typography variant="h6" id="modal-title">
-            {props.title}
+            {this.props.data[nasaid].title}
           </Typography>
-          <img src='https://images-assets.nasa.gov/image/jsc2007e034221/jsc2007e034221~thumb.jpg' alt={props.title} />
+          <img src={`https://images-assets.nasa.gov/image/${nasaid}/${nasaid}~thumb.jpg`} alt={this.props.title} />
           <Typography variant="subtitle1" id="simple-modal-description">
-            {props.description}
+            {this.props.data[nasaid].description}
           </Typography>
           </div>
         </Modal>
     )
 }
+}
 
-export default withStyles(styles)(SimpleModal);
+export default withStyles(styles)(withRouter(SimpleModal));
 
